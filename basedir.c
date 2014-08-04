163018f9ad4dfebf3a37d19791dde08be0e51d83
Stanislav Malyshev <smalyshev@gmail.com>
2014-08-03 21:37:52 -0700

    first checkin

---
+++ basedir.c
@@ -0,0 +1,145 @@
+/*
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2014 SugarCRM                                     |
+  +----------------------------------------------------------------------+
+  | Author: Stas Malyshev <smalyshev@sugarcrm.com>                       |
+  +----------------------------------------------------------------------+
+*/
+
+/* $Id$ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "php.h"
+#include "php_ini.h"
+#include "ext/standard/info.h"
+#include "php_basedir.h"
+#include "php_globals.h"
+#include "SAPI.h"
+
+ZEND_DECLARE_MODULE_GLOBALS(basedir)
+
+/* True global resources - no need for thread safety here */
+static int le_basedir;
+
+/* {{{ basedir_functions[]
+ *
+ * Every user visible function must have an entry in basedir_functions[].
+ */
+const zend_function_entry basedir_functions[] = {
+	PHP_FE_END	/* Must be the last line in basedir_functions[] */
+};
+/* }}} */
+
+/* {{{ basedir_module_entry
+ */
+zend_module_entry basedir_module_entry = {
+#if ZEND_MODULE_API_NO >= 20010901
+	STANDARD_MODULE_HEADER,
+#endif
+	"basedir",
+	basedir_functions,
+	PHP_MINIT(basedir),
+	PHP_MSHUTDOWN(basedir),
+	PHP_RINIT(basedir),		/* Replace with NULL if there's nothing to do at request start */
+	PHP_RSHUTDOWN(basedir),	/* Replace with NULL if there's nothing to do at request end */
+	PHP_MINFO(basedir),
+#if ZEND_MODULE_API_NO >= 20010901
+	PHP_BASEDIR_VERSION,
+#endif
+	STANDARD_MODULE_PROPERTIES
+};
+/* }}} */
+
+#ifdef COMPILE_DL_BASEDIR
+ZEND_GET_MODULE(basedir)
+#endif
+
+/* {{{ PHP_INI
+ */
+PHP_INI_BEGIN()
+    STD_PHP_INI_ENTRY("basedir.enabled",      "1", PHP_INI_ALL, OnUpdateBool, enabled, zend_basedir_globals, basedir_globals)
+PHP_INI_END()
+/* }}} */
+
+/* {{{ php_basedir_init_globals
+ */
+static void php_basedir_init_globals(zend_basedir_globals *basedir_globals)
+{
+	basedir_globals->enabled = 1;
+}
+/* }}} */
+
+/* {{{ PHP_MINIT_FUNCTION
+ */
+PHP_MINIT_FUNCTION(basedir)
+{
+	REGISTER_INI_ENTRIES();
+	return SUCCESS;
+}
+/* }}} */
+
+/* {{{ PHP_MSHUTDOWN_FUNCTION
+ */
+PHP_MSHUTDOWN_FUNCTION(basedir)
+{
+	UNREGISTER_INI_ENTRIES();
+	return SUCCESS;
+}
+/* }}} */
+
+/* Remove if there's nothing to do at request start */
+/* {{{ PHP_RINIT_FUNCTION
+ */
+PHP_RINIT_FUNCTION(basedir)
+{
+	if(PG(open_basedir) && *PG(open_basedir) && SG(request_info).path_translated && *SG(request_info).path_translated) {
+        int oblen = strlen(PG(open_basedir));
+        char *new_basedir = emalloc(oblen+2+strlen(SG(request_info).path_translated));
+        char *localpath;
+        strcpy(new_basedir, PG(open_basedir));
+        new_basedir[oblen] = ':';
+        strcpy(new_basedir+oblen+1, SG(request_info).path_translated);
+        localpath = strstr(SG(request_info).path_translated, SG(request_info).request_uri);
+        if(localpath) {
+            new_basedir[oblen+1+(localpath-SG(request_info).path_translated)] = '\0';
+        }
+        zend_alter_ini_entry("open_basedir", sizeof("open_basedir"), new_basedir, strlen(new_basedir)+1, ZEND_INI_SYSTEM, PHP_INI_STAGE_ACTIVATE);
+        efree(new_basedir);
+    }
+	return SUCCESS;
+}
+/* }}} */
+
+/* Remove if there's nothing to do at request end */
+/* {{{ PHP_RSHUTDOWN_FUNCTION
+ */
+PHP_RSHUTDOWN_FUNCTION(basedir)
+{
+	return SUCCESS;
+}
+/* }}} */
+
+/* {{{ PHP_MINFO_FUNCTION
+ */
+PHP_MINFO_FUNCTION(basedir)
+{
+	php_info_print_table_start();
+	php_info_print_table_header(2, "basedir support", "enabled");
+	php_info_print_table_end();
+
+	DISPLAY_INI_ENTRIES();
+}
+/* }}} */
+
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
---
+++ php_basedir.h
@@ -0,0 +1,69 @@
+/*
+  +----------------------------------------------------------------------+
+  | PHP Version 5                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2014 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_01.txt                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Author:                                                              |
+  +----------------------------------------------------------------------+
+*/
+
+/* $Id$ */
+
+#ifndef PHP_BASEDIR_H
+#define PHP_BASEDIR_H
+
+extern zend_module_entry basedir_module_entry;
+#define phpext_basedir_ptr &basedir_module_entry
+
+#define PHP_BASEDIR_VERSION "0.1.0" /* Replace with version number for your extension */
+
+#ifdef PHP_WIN32
+#	define PHP_BASEDIR_API __declspec(dllexport)
+#elif defined(__GNUC__) && __GNUC__ >= 4
+#	define PHP_BASEDIR_API __attribute__ ((visibility("default")))
+#else
+#	define PHP_BASEDIR_API
+#endif
+
+#ifdef ZTS
+#include "TSRM.h"
+#endif
+
+PHP_MINIT_FUNCTION(basedir);
+PHP_MSHUTDOWN_FUNCTION(basedir);
+PHP_RINIT_FUNCTION(basedir);
+PHP_RSHUTDOWN_FUNCTION(basedir);
+PHP_MINFO_FUNCTION(basedir);
+
+PHP_FUNCTION(confirm_basedir_compiled);	/* For testing, remove later. */
+
+ZEND_BEGIN_MODULE_GLOBALS(basedir)
+	zend_bool enabled;
+ZEND_END_MODULE_GLOBALS(basedir)
+
+#ifdef ZTS
+#define BASEDIR_G(v) TSRMG(basedir_globals_id, zend_basedir_globals *, v)
+#else
+#define BASEDIR_G(v) (basedir_globals.v)
+#endif
+
+#endif	/* PHP_BASEDIR_H */
+
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
